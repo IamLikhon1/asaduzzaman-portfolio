6edf87d6b16bd9b1af53bb5dc74568a33abd7c42
IamLikhon1 <likhonadnan55@gmail.com>
2023-07-01 12:14:07 +0600

    addded more project

--- src/pages/Home/Projects.jsx
+++ src/pages/Home/Projects.jsx
@@ -1,6 +1,9 @@
 import ProjectOne from '../../../public/projectOne.png'
 import ProjectTwo from '../../../public/projectTwo.png'
 import ProjectThree from '../../../public/projectThree.png'
+import projectFour from '../../../public/projectSix.png'
+import projectSix from '../../../public/projectSeven.png'
+import projectFive from '../../../public/projectFive.png'
 
 import { FiExternalLink, FiGithub } from "react-icons/fi";
 
@@ -101,6 +104,84 @@ const Projects = () => {
         </div>
         </div>
            {/* projectThree */}
+
+           {/* projectFour */}
+           <div className=" card-container card card-compact bg-base-100 shadow-xl">
+        <figure><img className='img-container' src={projectFour} alt="Shoes" /></figure>
+        <div className="card-body">
+            <h2 className="card-title">Figma To Html</h2>
+            <p className='text-xl my-3'>The Tea House</p>
+            <div className='grid grid-cols-2'>
+               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Website-Live-Link">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://iamlikhon1.github.io/tea-house-bs/"target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
+
+                </a>
+                </button>
+               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Client-Github-Site">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/tea-house-bs" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
+
+                </a>
+                </button>
+
+               
+            </div>
+            
+            
+        </div>
+        </div>
+           {/* projectFour */}
+
+            {/* projectFive */}
+            <div className=" card-container card card-compact bg-base-100 shadow-xl">
+        <figure><img className='img-container' src={projectSix} alt="Shoes" /></figure>
+        <div className="card-body">
+            <h2 className="card-title">Job Applied Site(Dynamic)</h2>
+            <p className='text-xl my-3'>WorkJet</p>
+            <div className='grid grid-cols-2'>
+               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Website-Live-Link">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://meek-starburst-bd6984.netlify.app/"target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
+
+                </a>
+                </button>
+               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Client-Github-Site">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/job-hunter" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
+
+                </a>
+                </button>
+
+               
+            </div>
+            
+            
+        </div>
+        </div>
+           {/* projectFive */}
+
+            {/* projectFive */}
+            <div className=" card-container card card-compact bg-base-100 shadow-xl">
+        <figure><img className='img-container' src={projectFive} alt="Shoes" /></figure>
+        <div className="card-body">
+            <h2 className="card-title">PSD To Html(Wedding Event)</h2>
+            <p className='text-xl my-3'>Abacus Convention Center</p>
+            <div className='grid grid-cols-2'>
+               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Website-Live-Link">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://iamlikhon1.github.io/convention-center/"target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
+
+                </a>
+                </button>
+               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Client-Github-Site">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/convention-center" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
+
+                </a>
+                </button>
+
+               
+            </div>
+            
+            
+        </div>
+        </div>
+           {/* projectFive */}
                 
             </div>
            {/* card */}
