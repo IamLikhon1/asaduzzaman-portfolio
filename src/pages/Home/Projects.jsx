4fcf6168a5b720af16d52eedbb6e5cf0237227c4
IamLikhon1 <likhonadnan55@gmail.com>
2023-07-25 12:06:36 +0600

    added new project

--- src/pages/Home/Projects.jsx
+++ src/pages/Home/Projects.jsx
@@ -19,7 +19,39 @@ const Projects = () => {
         data-aos-easing="linear"
         data-aos-duration="2000">
 
+            {/* projectOne */}
+            <div className=" card-container card card-compact bg-base-100 shadow-xl"data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="">
+        <figure><img className='img-container' src={projectSix} alt="Shoes" /></figure>
+        <div className="card-body">
+            <h2 className="card-title">College Booking Facilities (MERN)</h2>
+            <p className='text-xl my-3'>Collegiate Nexus</p>
+            <div className='grid grid-cols-3'>
+               <button className='mx-1 tooltip-warning mt-4 tooltip'data-tip="Website-Live-Link">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://collegiate-nexus.web.app/" target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
+
+                </a>
+                </button>
+               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Client-Github-Site">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/college-client" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
+
+                </a>
+                </button>
+
+               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Server-Github-Site">
+                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/college-server"target='blank'>Sever<FiGithub className='items-center mt-1 '></FiGithub>
+
+                </a>
+                </button>
+            </div>
+            
+            
+        </div>
+        </div>
            {/* projectOne */}
+
+           {/* projectTwo */}
            <div className=" card-container card card-compact bg-base-100 shadow-xl">
         <figure><img className='img-container' src={ProjectOne} alt="Shoes" /></figure>
         <div className="card-body">
@@ -47,9 +79,9 @@ const Projects = () => {
             
         </div>
         </div>
-           {/* projectOne */}
-
            {/* projectTwo */}
+
+           {/* projectThree */}
            <div className=" card-container card card-compact bg-base-100 shadow-xl">
         <figure><img className='img-container' src={ProjectTwo} alt="Shoes" /></figure>
         <div className="card-body">
@@ -73,10 +105,12 @@ const Projects = () => {
             
         </div>
         </div>
-           {/* projectTwo */}
-
            {/* projectThree */}
-           <div className=" card-container card card-compact bg-base-100 shadow-xl">
+
+           {/* projectFour */}
+           <div className=" card-container card card-compact bg-base-100 shadow-xl"data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="2000">
         <figure><img className='img-container' src={ProjectThree} alt="Shoes" /></figure>
         <div className="card-body">
             <h2 className="card-title">Toy Market (MERN)</h2>
@@ -103,9 +137,9 @@ const Projects = () => {
             
         </div>
         </div>
-           {/* projectThree */}
-
            {/* projectFour */}
+
+           {/* projectFive */}
            <div className=" card-container card card-compact bg-base-100 shadow-xl"data-aos="fade-down"
         data-aos-easing="linear"
         data-aos-duration="2000">
@@ -131,35 +165,9 @@ const Projects = () => {
             
         </div>
         </div>
-           {/* projectFour */}
-
-            {/* projectFive */}
-            <div className=" card-container card card-compact bg-base-100 shadow-xl"data-aos="fade-down"
-        data-aos-easing="linear"
-        data-aos-duration="2000">
-        <figure><img className='img-container' src={projectSix} alt="Shoes" /></figure>
-        <div className="card-body">
-            <h2 className="card-title">Job Applied Site(Dynamic)</h2>
-            <p className='text-xl my-3'>WorkJet</p>
-            <div className='grid grid-cols-2'>
-               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Website-Live-Link">
-                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://meek-starburst-bd6984.netlify.app/"target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
-
-                </a>
-                </button>
-               <button className='mx-1 tooltip mt-4 tooltip-warning'data-tip="Client-Github-Site">
-                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/job-hunter" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
-
-                </a>
-                </button>
-
-               
-            </div>
-            
-            
-        </div>
-        </div>
            {/* projectFive */}
+
+            
 
             {/* projectSix*/}
             <div className=" card-container card card-compact bg-base-100 shadow-xl"data-aos="fade-down"
