a4e713419865ea84ead732a2e9e9f5f8338be39c
IamLikhon1 <likhonadnan55@gmail.com>
2023-11-30 19:37:00 +0600

    modify new project section

--- src/pages/Home/Projects.jsx
+++ src/pages/Home/Projects.jsx
@@ -1,219 +1,107 @@
-import ProjectOne from '../../../public/projectOne.png'
-import ProjectTwo from '../../../public/projectTwo.png'
-import ProjectThree from '../../../public/projectThree.png'
-import projectFour from '../../../public/projectSix.png'
-import projectSix from '../../../public/projectSeven.png'
-import projectFive from '../../../public/projectFive.png'
-
-import { FiExternalLink, FiGithub } from "react-icons/fi";
 import { useEffect, useState } from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import SingleProject from './ProjectsLits/SingleProject';
 
 
 const Projects = () => {
-    const [allData, setAllData]=useState([]);
+    const [allData, setAllData] = useState([]);
 
-    const fullStackProjects= allData.filter(item=>item.category=='FullStack');
-    const frontendProjects= allData.filter(item=>item.category=='Frontend');
-    const teamProjects= allData.filter(item=>item.category=='Team');
-    const figmaToHtml= allData.filter(item=>item.category=='Figma');
+    const fullStackProjects = allData.filter(item => item.category == 'FullStack');
+    const frontendProjects = allData.filter(item => item.category == 'Frontend');
+    const teamProjects = allData.filter(item => item.category == 'Team');
+    const figmaToHtml = allData.filter(item => item.category == 'Figma');
     // console.log(teamProjects);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('project.json')
-        .then(res=>res.json())
-        .then(data=>setAllData(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setAllData(data))
+    }, [])
     return (
-        <div id="Project" className="mx-10 md:p-5" data-aos="fade-down"
-            data-aos-easing="linear"
-            data-aos-duration="1500">
+        <div id="Project" className="mx-10 md:p-5">
             <h2 className="text-4xl border-l-4  border-orange-400"><span className='ml-2'>Projects</span></h2>
-            {/* card */}
-            <div className="grid md:grid-cols-3 gap-5 mt-10" data-aos="fade-down"
-                data-aos-easing="linear"
-                data-aos-duration="2000">
 
+            <Tabs>
 
-                {/* projectOne */}
-                <div className="card-container card card-compact  bg-base-100 shadow-xl" data-aos="fade-down"
-                    data-aos-easing="linear"
-                    data-aos-duration="">
-                    <figure><img className='img-container' src={projectSix} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">College Booking Facilities (MERN)</h2>
-                        <p className='text-xl my-3'>Collegiate Nexus</p>
-                        <div className='grid grid-cols-3'>
-                            <button className='mx-1 tooltip-warning mt-4 tooltip' data-tip="Website-Live-Link">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://collegiate-nexus.web.app/" target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
+                <TabList className="mt-5 mb-12 md:my-12 md:mb-20 md:flex justify-center gap-10 ">
+                    {/* All Tab */}
+                    <Tab className="text-xl cursor-pointer px-10 py-5  my-2 md:my-0 text-center duration-500 border-2 rounded-md shadow-lg font-medium flex items-center justify-between hover:text-orange-500 focus:text-orange-600 focus:border-orange-500">ALL Project <span className='px-3 py-2 bg-orange-400 text-white rounded-full ml-5 font-semibold text-sm'>{allData.length} </span></Tab>
+                    {/* Full-Stack Tab */}
+                    <Tab className="text-xl cursor-pointer px-10 py-5  my-2 md:my-0 text-center duration-500 border-2 rounded-md shadow-lg font-medium  flex items-center justify-between hover:text-orange-500 focus:text-orange-600 focus:border-orange-500">Full Stack Project <span className='px-4 py-2 bg-orange-400 text-white rounded-full ml-5 font-semibold text-sm'>{fullStackProjects.length} </span>
+                    </Tab>
+                    {/* Front-End Tab */}
+                    <Tab className="text-xl cursor-pointer px-10 py-5  my-2 md:my-0 text-center duration-500 border-2 rounded-md shadow-lg font-medium  flex items-center justify-between hover:text-orange-500 focus:text-orange-600 focus:border-orange-500">Front-End Project <span className='px-4 py-2 bg-orange-400 text-white rounded-full ml-5 font-semibold text-sm'>{frontendProjects.length} </span>
+                    </Tab>
 
-                                </a>
-                            </button>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Client-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/college-client" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
+                    {/* Figma to Html Tab */}
+                    <Tab className="text-xl cursor-pointer px-10 py-5  my-2 md:my-0 text-center duration-500 border-2 rounded-md shadow-lg font-medium  flex items-center justify-between hover:text-orange-500 focus:text-orange-600 focus:border-orange-500">Figma to HTML Project <span className='px-4 py-2 bg-orange-400 text-white rounded-full ml-5 font-semibold text-sm'>{figmaToHtml.length} </span>
+                    </Tab>
+                    {/* Team Tab */}
+                    <Tab className="text-xl cursor-pointer px-10 py-5  my-2 md:my-0 text-center duration-500 border-2 rounded-md shadow-lg font-medium  flex items-center justify-between hover:text-orange-500 focus:text-orange-600 focus:border-orange-500">Team Work Project <span className='px-4 py-2 bg-orange-400 text-white rounded-full ml-5 font-semibold text-sm'>{teamProjects.length} </span>
+                    </Tab>
+                </TabList>
 
-                                </a>
-                            </button>
+                {/* All Project */}
+                <TabPanel>
+                   <div className='grid md:grid-cols-4 md:gap-10  my-5 md:my-0'>
+                   {
+                        allData.map(data => <SingleProject
+                            key={data.id}
+                            data={data}
+                        ></SingleProject>)
+                    }
+                   </div>
+                </TabPanel>
+                {/* Full-Stack-Project */}
+                <TabPanel>
+                <div className='grid md:grid-cols-3 md:gap-10  my-5 md:my-0'>
+                   {
+                        fullStackProjects.map(data => <SingleProject
+                            key={data.id}
+                            data={data}
+                        ></SingleProject>)
+                    }
+                   </div>
+                </TabPanel>
 
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Server-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/college-server" target='blank'>Sever<FiGithub className='items-center mt-1 '></FiGithub>
+                {/* Front-End Project */}
+                <TabPanel>
+                <div className='grid md:grid-cols-3 md:gap-10  my-5 md:my-0'>
+                   {
+                        frontendProjects.map(data => <SingleProject
+                            key={data.id}
+                            data={data}
+                        ></SingleProject>)
+                    }
+                   </div>
+                </TabPanel>
 
-                                </a>
-                            </button>
-                        </div>
+                {/* Figma to Html project */}
+                <TabPanel>
+                <div className='grid md:grid-cols-3 md:gap-10  my-5 md:my-0'>
+                   {
+                        figmaToHtml.map(data => <SingleProject
+                            key={data.id}
+                            data={data}
+                        ></SingleProject>)
+                    }
+                   </div>
+                </TabPanel>
 
+                {/* team project */}
+                <TabPanel>
+                <div className='grid md:grid-cols-3 md:gap-10  my-5 md:my-0'>
+                   {
+                        teamProjects.map(data => <SingleProject
+                            key={data.id}
+                            data={data}
+                        ></SingleProject>)
+                    }
+                   </div>
+                </TabPanel>
 
-                    </div>
-                </div>
-                {/* projectOne */}
-
-                {/* projectTwo */}
-                <div className=" card-container card card-compact bg-base-100 shadow-xl">
-                    <figure><img className='img-container' src={ProjectOne} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Foreign Language (MERN)</h2>
-                        <p className='text-xl my-3'>Summer School Camp Website</p>
-                        <div className='grid grid-cols-3'>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Website-Live-Link">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://fashion-summer.web.app/" target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
-
-                                </a>
-                            </button>
-                            <button className='mx-1 tooltip mt-4  tooltip-warning' data-tip="Client-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/language-vacation" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
-
-                                </a>
-                            </button>
-
-                            <button className='mx-1 tooltip mt-4  tooltip-warning' data-tip="Server-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/language-server" target='blank'>Sever<FiGithub className='items-center mt-1 '></FiGithub>
-
-                                </a>
-                            </button>
-                        </div>
-
-
-                    </div>
-                </div>
-                {/* projectTwo */}
-
-                {/* projectThree */}
-                <div className=" card-container card card-compact bg-base-100 shadow-xl">
-                    <figure><img className='img-container' src={ProjectTwo} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Parlour Website</h2>
-                        <p className='text-xl my-3'>Simple Parlour(Authentication)</p>
-                        <div className='grid grid-cols-2'>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Website-Live-Link">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://simple-parlour-84df8.web.app/" target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
-
-                                </a>
-                            </button>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Client-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/simple-parlour" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
-
-                                </a>
-                            </button>
-
-
-                        </div>
-
-
-                    </div>
-                </div>
-                {/* projectThree */}
-
-                {/* projectFour */}
-                <div className=" card-container card card-compact bg-base-100 shadow-xl" data-aos="fade-down"
-                    data-aos-easing="linear"
-                    data-aos-duration="2000">
-                    <figure><img className='img-container' src={ProjectThree} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Toy Market (MERN)</h2>
-                        <p className='text-xl my-3'>Toy-Cars-World</p>
-                        <div className='grid grid-cols-3'>
-                            <button className='mx-1 tooltip-warning mt-4 tooltip' data-tip="Website-Live-Link">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://toys-car.web.app/" target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
-
-                                </a>
-                            </button>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Client-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/markect-toy" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
-
-                                </a>
-                            </button>
-
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Server-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/toy-market-server" target='blank'>Sever<FiGithub className='items-center mt-1 '></FiGithub>
-
-                                </a>
-                            </button>
-                        </div>
-
-
-                    </div>
-                </div>
-                {/* projectFour */}
-
-                {/* projectFive */}
-                <div className=" card-container card card-compact bg-base-100 shadow-xl" data-aos="fade-down"
-                    data-aos-easing="linear"
-                    data-aos-duration="2000">
-                    <figure><img className='img-container' src={projectFour} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Figma To Html</h2>
-                        <p className='text-xl my-3'>The Tea House</p>
-                        <div className='grid grid-cols-2'>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Website-Live-Link">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://iamlikhon1.github.io/tea-house-bs/" target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
-
-                                </a>
-                            </button>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Client-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/tea-house-bs" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
-
-                                </a>
-                            </button>
-
-
-                        </div>
-
-
-                    </div>
-                </div>
-                {/* projectFive */}
-
-
-
-                {/* projectSix*/}
-                <div className=" card-container card card-compact bg-base-100 shadow-xl" data-aos="fade-down"
-                    data-aos-easing="linear"
-                    data-aos-duration="2000">
-                    <figure><img className='img-container' src={projectFive} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">PSD To Html(Wedding Event)</h2>
-                        <p className='text-xl my-3'>Abacus Convention Center</p>
-                        <div className='grid grid-cols-2'>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Website-Live-Link">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://iamlikhon1.github.io/convention-center/" target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
-
-                                </a>
-                            </button>
-                            <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Client-Github-Site">
-                                <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/convention-center" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
-
-                                </a>
-                            </button>
-
-
-                        </div>
-
-
-                    </div>
-                </div>
-                {/* projectSix */}
-
-            </div>
-            {/* card */}
+            </Tabs>
 
         </div>
     );
--- src/pages/Home/ProjectsLits/SingleProject.jsx
+++ src/pages/Home/ProjectsLits/SingleProject.jsx
@@ -1,29 +1,28 @@
 import { FiExternalLink, FiGithub } from "react-icons/fi";
 
-const SingleProject = () => {
+const SingleProject = ({ data }) => {
+    const { img, title, sunTitle, liveLink, client, server} = data;
     return (
         <div>
-            <div className="card-container card card-compact  bg-base-100 shadow-xl" data-aos="fade-down"
-                data-aos-easing="linear"
-                data-aos-duration="">
-                <figure><img className='img-container' src={"projectSix"} alt="Shoes" /></figure>
+            <div className="card-container card card-compact bg-base-200  cursor-pointer shadow-md hover:shadow-lg duration-500 my-5 md:my-0 w-fit">
+                <figure><img className='img-container' src={img} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">College Booking Facilities (MERN)</h2>
-                    <p className='text-xl my-3'>Collegiate Nexus</p>
+                    <h2 className="card-title">{title}</h2>
+                    <p className='text-xl my-3'>{sunTitle}</p>
                     <div className='grid grid-cols-3'>
                         <button className='mx-1 tooltip-warning mt-4 tooltip' data-tip="Website-Live-Link">
-                            <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://collegiate-nexus.web.app/" target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
+                            <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href={liveLink} target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
 
                             </a>
                         </button>
                         <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Client-Github-Site">
-                            <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/college-client" target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
+                            <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href={client} target='blank'>Client<FiGithub className='items-center mt-1 '></FiGithub>
 
                             </a>
                         </button>
 
                         <button className='mx-1 tooltip mt-4 tooltip-warning' data-tip="Server-Github-Site">
-                            <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href="https://github.com/IamLikhon1/college-server" target='blank'>Sever<FiGithub className='items-center mt-1 '></FiGithub>
+                            <a className='flex items-center text-lg font-semibold hover:text-orange-400 duration-500' href={server} target='blank'>Sever<FiGithub className='items-center mt-1 '></FiGithub>
 
                             </a>
                         </button>
