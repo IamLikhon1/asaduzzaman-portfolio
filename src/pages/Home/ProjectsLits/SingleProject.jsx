41281ab5241b96ab591727fe8be73ba3cc721cd7
IamLikhon1 <likhonadnan55@gmail.com>
2023-12-03 12:04:50 +0600

    modify project section

--- src/pages/Home/ProjectsLits/SingleProject.jsx
+++ src/pages/Home/ProjectsLits/SingleProject.jsx
@@ -7,9 +7,10 @@ const SingleProject = ({ data }) => {
             <div className="card-container card card-compact bg-base-200  cursor-pointer shadow-md hover:shadow-lg duration-500 my-3 md:my-0 w-fit">
                 <figure><img className='img-container' src={img} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
-                    <p className='text-xl my-2'>{sunTitle}</p>
-                    <div className='grid grid-cols-3'>
+                    <h2 className="text-2xl font-medium">{title}</h2>
+                    <p className='text-lg'>{sunTitle}</p>
+
+                    <div className='flex justify-between items-center mx-2'>
                         <button className='mx-1 tooltip-warning mt-3 tooltip' data-tip="Website-Live-Link">
                             <a className='flex gap-2 items-center text-lg font-semibold hover:text-orange-400 duration-500' href={liveLink} target='blank'>Live Link <FiExternalLink className='items-center mt-1'></FiExternalLink>
 
